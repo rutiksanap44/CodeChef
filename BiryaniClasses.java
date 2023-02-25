0877daf17840fdaa7c1ed927d6157abd6a9fe5cc
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-02-25 22:25:46 +0530

    Water Consuption

---
+++ BiryaniClasses.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class BiryaniClasses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while(tc>0){
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            System.out.println(n1*n2);
+            tc--;
+        }
+    }
+}
